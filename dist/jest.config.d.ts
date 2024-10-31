212b10ddf1585db103847db69513e20ccfb10588
Big Fat Cow <kascow.io@gmail.com>
2024-10-31 19:04:48 +0200

    feat: removed dist

--- dist/jest.config.d.ts
+++
@@ -1,9 +0,0 @@
-declare const _default: {
-    clearMocks: boolean;
-    collectCoverage: boolean;
-    coverageDirectory: string;
-    preset: string;
-    rootDir: string;
-    testEnvironment: string;
-};
-export default _default;
